e5c932c570cb90537f60eb94ccbf4a54f1444d8c
MoreliaReyesC <morelia.reyes.ti@gmail.com>
2025-09-04 16:43:14 -0400

    modificaciones al archivo app.js

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// El principal objetivo de este desafío es fortalecer tus habilidades 
+// en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//archivo de js con todas las funcionalidades de amigo secreto
+
+//variables
+let listaAmigos = [];
+var capturaAmigo = "";
+
+function agregarAmigo()
+{
+ capturaAmigo = document.getElementById("amigo").value;
+ listaAmigos.push(capturaAmigo);
+ alert("El amigo ingresado es "+capturaAmigo);
+ document.getElementById("amigo").value = "";
+
+ 
+}
+
+
+function sortearAmigo()
+{
+alert("sortear amigo secreto "+listaAmigos);
+var randomListaAmigos = Math.floor(Math.random() * listaAmigos.length);
+var randomValorListaAmigos = listaAmigos[randomListaAmigos];
+alert("sorteado " +randomValorListaAmigos);
+
+}
