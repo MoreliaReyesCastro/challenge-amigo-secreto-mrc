edf70db3b7490a96b4947ab29ec95a17266d285c
MoreliaReyesC <morelia.reyes.ti@gmail.com>
2025-09-05 02:34:26 -0400

    modificaciones al archivo app.js para metodos agregarAmigo y sortearAmigo

--- app.js
+++ app.js
@@ -8,20 +8,42 @@ var capturaAmigo = "";
 
 function agregarAmigo()
 {
- capturaAmigo = document.getElementById("amigo").value;
- listaAmigos.push(capturaAmigo);
- alert("El amigo ingresado es "+capturaAmigo);
- document.getElementById("amigo").value = "";
+ capturaAmigo = document.getElementById("amigo").value; //valor introducido en textbox
+ const lista = document.getElementById("listaAmigos"); //variable que aloja la lista
 
- 
+if (capturaAmigo.trim() !== "") {
+    const nuevoLi = document.createElement("li");
+    nuevoLi.textContent = capturaAmigo;
+    lista.appendChild(nuevoLi);
+    listaAmigos.push(capturaAmigo);
+    document.getElementById("amigo").value = "";
+    
+ } else {
+                alert("Por favor :), escribe un amigo para agregar.");
+            }
+
 }
 
 
 function sortearAmigo()
 {
+    
+const listaResultado = document.getElementById("resultado"); //variable que aloja la lista resultado
+const listaSorteo = document.getElementById("listaAmigos");
+
 alert("sortear amigo secreto "+listaAmigos);
 var randomListaAmigos = Math.floor(Math.random() * listaAmigos.length);
 var randomValorListaAmigos = listaAmigos[randomListaAmigos];
+const lista = document.getElementById("amigo");
 alert("sorteado " +randomValorListaAmigos);
 
+const resultadoLi = document.createElement("li");
+resultadoLi.textContent = "Elegido "+randomValorListaAmigos;
+listaResultado.appendChild(resultadoLi);
+
+
 }
+
+
+
+
